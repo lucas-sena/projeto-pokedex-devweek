2fbe0d7c300bfb21db043078c5231e184fea9a56
Lucas Sena <tejo.lucas13@gmail.com>
2022-05-05 17:32:54 -0300

    atualizando arquivo index.js

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,22 @@ listSelectionPokemon.forEach(pokemon => {
     // vamos precisar trabalhar com o evento clique feito pelo usuário na LISTAGEM
     pokemon.addEventListener('click', () => {
     // remover classe aberto do cartão que está aberto
+    const cartaoPokemonAberto = document.querySelector('.aberto')
+    cartaoPokemonAberto.classList.remove('.aberto')
     
+    //ao clicar em um pokémon da listagem pegamos o id desse pokémon pra saber qual cartão mostrar
+    const idPokemonSelecionado = pokemon.attributes.id.value
+    
+    const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+    const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+    cartaoPokemonParaAbrir.classList.add('aberto')
+
+    //remover a classe ativo que marca o pokémon selecionado
+    const pokemonAtivoNaListagem = document.querySelector('.ativo')
+    pokemonAtivoNaListagem.classList.remove('ativo')
+    
+    //adicionar a classe ativo no item da lista selecionado 
+    const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+    pokemonSelecionadoNaListagem.classList.add('ativo')
     })
 })
